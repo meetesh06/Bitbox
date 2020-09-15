caa3a9eb0ef0905ac6f8228c6207aa9f34271cdd
Kalpesh Mehta <kalpesh@Kalpeshs-iMac.local>
2020-09-15 22:46:28 +0530

    Init screen text fonts imported

--- screens/Init.js
+++ screens/Init.js
@@ -7,27 +7,45 @@ import AnimatedLinearGradient, {
 const App: () => React$Node = () => {
   return (
     <>
-      <AnimatedLinearGradient
-        customColors={gradientStyles.bitbox}
-        speed={4000}>
-        <Text>SLIM STHAT</Text>
+      <AnimatedLinearGradient customColors={gradientStyles.bitbox} speed={8000}>
+        <View
+          style={{
+            flex: 1,
+          }}
+        />
+
+        <View style={styles.textContainer}>
+          <Text style={styles.bigHeader}>Hello.</Text>
+          <Text style={styles.bigSubHeader}>Secure Encrypt Store</Text>
+        </View>
+
+        <View
+          style={{
+            flex: 1,
+          }}
+        />
       </AnimatedLinearGradient>
     </>
   );
 };
 
-// const styles = StyleSheet.create({
-//   scrollView: {
-//     backgroundColor: '#fff',
-//   },
-// });
+const styles = StyleSheet.create({
+  bigHeader: {
+    fontFamily: 'Poppins-ExtraBold',
+    fontSize: 30,
+    textAlign: 'center',
+    color: '#fff',
+  },
+  bigSubHeader: {
+    fontFamily: 'Poppins-Thin',
+    fontSize: 20,
+    color: '#fff',
+    textAlign: 'center',
+  },
+});
 
 const gradientStyles = {
-  bitbox: [
-    'rgb(57, 119, 222)',
-    'rgb(10, 147, 142)',
-    'rgb(29, 52, 134)'
-  ]
+  bitbox: ['rgb(57, 119, 222)', 'rgb(10, 147, 142)', 'rgb(29, 52, 134)'],
 };
 
 export default App;
