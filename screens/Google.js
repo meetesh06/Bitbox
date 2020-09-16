cf1ab5f2aa6f2b52f6c5732cd3e686ae48ea93d5
Kalpesh Mehta <kalpesh@Kalpeshs-iMac.local>
2020-09-17 01:52:45 +0530

    Integrate google Oauth2 API for google dirve

--- screens/Google.js
+++ screens/Google.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   StyleSheet,
   View,
@@ -10,8 +10,48 @@ import {
 } from 'react-native';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import {Hideo} from 'react-native-textinput-effects';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin';
+
 const App: () => React$Node = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive.appdata'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId:
+        '90184904422-urjbjla8slor0c0qh3e6nvir4htdn60h.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      // hostedDomain: '', // specifies a hosted domain restriction
+      // loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+      forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+      // accountName: '', // [Android] specifies an account name on the device that should be used
+      // iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+    });
+  }, []);
+
   function handleSignup() {}
+  async function googleSignIn() {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      // this.setState({userInfo});
+      console.log(userInfo);
+    } catch (error) {
+      console.log(error);
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  }
+
   return (
     <>
       <View style={styles.container}>
@@ -36,7 +76,7 @@ const App: () => React$Node = () => {
           </View>
           <View style={styles.submitContainer}>
             <View style={styles.buttonContainer}>
-              <TouchableOpacity style={styles.button4} onPress={handleSignup}>
+              <TouchableOpacity style={styles.button4} onPress={googleSignIn}>
                 <Text style={styles.button4Text}>Sign In with Google</Text>
               </TouchableOpacity>
               <Text style={styles.buttonSeperatorText}>OR</Text>
@@ -141,12 +181,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     margin: 20,
   },
-    buttonSeperatorText: {
+  buttonSeperatorText: {
     padding: 10,
     fontFamily: 'Poppins-Thin',
     color: '#052a37',
   },
-
 });
 
 export default App;
