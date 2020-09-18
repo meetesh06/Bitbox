3759f21ee225c59afa224e869498b0082fa9cf52
Kalpesh Mehta <kalpesh@Kalpeshs-iMac.local>
2020-09-18 17:00:42 +0530

    FAB update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,21 +7,55 @@ import {
   // Dimensions,
 } from 'react-native';
 import {darkThemeColor} from './Globals/Functions';
-import {B_CONTAINER, PRIMARY} from './Globals/Colors';
-import FAB from 'react-native-fab';
+import {B_CONTAINER, PRIMARY, WHITE, ACCENT} from './Globals/Colors';
+// import FAB from 'react-native-fab';
+import Fab from 'rn-fab';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import {useNavigation} from 'react-native-navigation-hooks';
 import {useNavigationCommandComplete} from 'react-native-navigation-hooks';
 
 const App: () => React$Node = () => {
-  const {showOverlay} = useNavigation();
+  const {showOverlay, showModal} = useNavigation();
   const [overlay, setOverlay] = useState(false);
+  const actions = [
+    // main button
+    {
+      icon: <FontAwesomeIcon name="plus" size={15} color={WHITE} />,
+      name: 'btn_plus',
+      color: PRIMARY,
+    },
+    {
+      text: 'Others',
+      icon: <FontAwesomeIcon name="pencil" color={WHITE} />,
+      name: 'credential',
+      color: ACCENT,
+    },
+    {
+      text: 'Credential',
+      icon: <FontAwesomeIcon name="unlock-alt" color={WHITE} />,
+      name: 'credential',
+      color: ACCENT,
+    },
+  ];
 
   useNavigationCommandComplete(({commandName}) => {
     if (commandName === 'dismissOverlay') {
       setOverlay(false);
     }
   });
+
+  function navigateCredential() {
+    showModal({
+      component: {
+        name: 'com.mk2er.CreateCredential',
+        options: {
+          topBar: {
+            visible: false,
+          },
+        },
+      },
+    });
+  }
 
   function showCreateOverlay() {
     setOverlay(true);
@@ -74,12 +108,23 @@ const App: () => React$Node = () => {
           Home Screen
         </Text>
       </ScrollView>
-      <FAB
+      {/* <FAB
         buttonColor={PRIMARY}
         iconTextColor="#FFFFFF"
         onClickAction={showCreateOverlay}
         visible={!overlay}
         iconTextComponent={<FontAwesomeIcon name="pencil" />}
+      /> */}
+      <Fab
+        actions={actions}
+        style={{right: 40, bottom: 80}}
+        rotation={'45deg'}
+        onPress={(name) => {
+          if (name === 'credential') {
+            navigateCredential();
+          }
+          // showCreateOverlay();
+        }}
       />
     </>
   );
